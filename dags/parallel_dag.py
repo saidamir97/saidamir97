9f3a31115016c1f0b7ecf8172bb2f8523b5a58cc
Saidamir Marufkhudjaev <saidkhudjaev@gmail.com>
2021-07-02 10:37:50 +0500

    initial commit

--- dags/parallel_dag.py
+++ dags/parallel_dag.py
@@ -1,5 +1,7 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.subdag import SubDagOperator
+from airflow.utils.task_group import TaskGroup
 
 from datetime import datetime
 
@@ -13,19 +15,20 @@ with DAG('parallel_dag', schedule_interval = '@daily', default_args = default_ar
         bash_command = 'sleep 3'
     )
 
-    task_2 = BashOperator(
+    with TaskGroup('processing_tasks') as processing_tasks:
+        task_2 = BashOperator(
         task_id = 'task_2',
         bash_command = 'sleep 3'
     )
 
-    task_3 = BashOperator(
-        task_id = 'task_3',
-        bash_command = 'sleep 3'
-    )
+        task_3 = BashOperator(
+            task_id = 'task_3',
+            bash_command = 'sleep 3'
+        )
 
     task_4 = BashOperator(
         task_id = 'task_4',
         bash_command = 'sleep 3'
     )
 
-    task_1 >> [task_2, task_3] >> task_4
+    task_1 >> processing_tasks >> task_4
